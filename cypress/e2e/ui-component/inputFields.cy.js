3c87e10ef1fafe69c8492a1e48e57302f5ae9ddd
Nguyen Thi Anh Thu <thuthudia123@gmail.com>
2025-09-01 21:39:46 +0700

    #comment radio button

--- cypress/e2e/ui-component/inputFields.cy.js
+++ cypress/e2e/ui-component/inputFields.cy.js
@@ -7,6 +7,16 @@ beforeEach('Open application',() => {
 it.only('input fields', () => {
     cy.contains('Forms').click()
     cy.contains('Form Layouts').click()
-    cy.get('#inputEmail1').type('hello@test.com', {delay:200}).clear().type('hello')
-    cy.contains('nb-card', 'Using the Grid').contains('Email').type('Yes It works')
+    const name = 'Artem'
+    cy.get('#inputEmail1').type('hello@test.com', {delay:200}).clear().type('hello').clear()
+    cy.contains('nb-card', 'Using the Grid').contains('Email').type(`${name}@test.com`)
+
+    cy.get('#inputEmail1').should('have.value', `${name}@test.com`).clear().type('test@bondaracademy.com').press(Cypress.Keyboard.Keys.TAB)
+
+    cy.contains('Auth').click()
+    cy.contains('Login').click()
+
+    cy.get('#input-email').type('test@bondaracademy.com')
+    cy.get('#input-password').type('Welcome{enter}')
+
 })
--- cypress/e2e/ui-component/radioButtons.cy.js
+++ cypress/e2e/ui-component/radioButtons.cy.js
@@ -5,5 +5,14 @@ beforeEach('Open application',() => {
 })
 
 it.only('radio button', () => {
-    
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+    cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then(allRadioButtons  => {
+        cy.wrap(allRadioButtons).eq(0).check({force:true}).should('be.checked')
+        cy.wrap(allRadioButtons).eq(1).should('not.be.checked')
+        cy.wrap(allRadioButtons).eq(2).should('be.disabled')
+    })
+
+    // cy.contains('nb-card', 'Using the Grid').contains('Option 1').find('[type="radio"]').check({force:true}).should('be.checked')
+    cy.contains('nb-card', 'Using the Grid').contains('label','Option 1').find('input').click({force:true})
 })
