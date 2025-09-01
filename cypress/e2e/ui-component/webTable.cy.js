6828b47fca925e17c9ea5002362620edd084abdc
Nguyen Thi Anh Thu <thuthudia123@gmail.com>
2025-09-01 23:39:30 +0700

    #comment web table

--- cypress/e2e/ui-component/webTable.cy.js
+++ cypress/e2e/ui-component/webTable.cy.js
@@ -5,5 +5,44 @@ beforeEach('Open application',() => {
 })
 
 it.only('web table', () => {
+    cy.contains('Tables & Data').click()
+    cy.contains('Smart Table').click()
     
-})
+
+    //1. How to find by text
+    // cy.get('tbody').contains('tr', 'Larry').then(tableRow => {
+    //     cy.wrap(tableRow).find('.nb-edit').click()
+    //     cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('35')
+    //     cy.wrap(tableRow).find('.nb-checkmark').click()
+    //     cy.wrap(tableRow).find('td').last().should('have.text', '35')
+
+    // //2. How to find by index
+    // cy.get('.nb-plus').click()
+    // cy.get('thead tr').eq(2).then(tableRow => {
+    //     cy.wrap(tableRow).find('[placeholder="First Name"]').type('John')
+    //     cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Smith')
+    //     cy.wrap(tableRow).find('.nb-checkmark').click()
+
+    //     })
+    //     cy.get('tbody tr').first().find('td').then(tableColumns => {
+    //         cy.wrap(tableColumns).eq(2).should('have.text', 'John')
+    //         cy.wrap(tableColumns).eq(3).should('have.text', 'Smith')
+    //     })
+     
+        //3. Looping though the rows
+
+    const ages = [20, 30, 40, 200]
+
+    cy.wrap(ages).each(age => {
+        cy.get('[placeholder="Age"]').clear().type(age)
+        cy.wait(500)
+        cy.get('tbody tr').each(tableRows => {
+            if (age == 200) {
+                cy.wrap(tableRows).should('contain.text', 'No data found')
+            } else {
+                cy.wrap(tableRows).find('td').last().should('have.text', age)
+            }
+        })
+    })
+
+    })
